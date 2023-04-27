9f6c8fa833e663741d1245cfb61471e68f7be7ce
AARSHBAGAWADE <125872218+AARSHBAGAWADE@users.noreply.github.com>
2023-04-27 11:44:08 +0530

    Add files via upload

---
+++ task1part2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 11:38:37 2023
+
+@author: aarsh
+"""
+
+filename = input("Input the Filename: ")
+extension = filename.split(".")[-1]
+print("The extension of the file is :", extension)
