b25d6c8e7bd61e71f101c3d6a1ed39f094f910a7
AARSHBAGAWADE <125872218+AARSHBAGAWADE@users.noreply.github.com>
2023-04-27 11:38:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 11:32:26 2023
+
+@author: aarsh
+"""
+
+from math import pi
+
+radius = float(input("Input the radius of the circle: "))
+area = pi * radius**2
+
+print(f"The area of the circle with radius {radius} is: {area}")
