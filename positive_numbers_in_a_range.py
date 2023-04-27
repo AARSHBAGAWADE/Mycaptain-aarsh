472c04f6d45551f4d7cb1170609eff57ccf35db0
AARSHBAGAWADE <125872218+AARSHBAGAWADE@users.noreply.github.com>
2023-04-27 12:21:48 +0530

    Add files via upload

---
+++ positive_numbers_in_a_range.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 12:17:53 2023
+
+@author: aarsh
+"""
+
+# Python program to print positive Numbers in a List
+  
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+       
+# Python program to print positive Numbers in a List
+  
+# list of numbers
+list2 = [12, 14, -95, 3]
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
